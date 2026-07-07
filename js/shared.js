b685ccfd7ffdd95a1b083df5bd4836da9e5df60e
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-07-08 00:07:16 +0200

    Set up login and signup structure

---
+++ js/shared.js
@@ -0,0 +1,13 @@
+﻿function getStoredUser() {
+  return JSON.parse(localStorage.getItem('joinUser'));
+}
+
+
+function saveStoredUser(user) {
+  localStorage.setItem('joinUser', JSON.stringify(user));
+}
+
+
+function clearStoredUser() {
+  localStorage.removeItem('joinUser');
+}
