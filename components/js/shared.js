d6524ffa4c750b0b32b4b2d4618b3a3d761f80bd
Jonas Hildebrand <jhildebrand94@googlemail.com>
2026-07-14 14:09:58 +0200

    docs: add JSDoc to shared helpers

--- components/js/shared.js
+++ components/js/shared.js
@@ -1,18 +1,35 @@
-﻿function getStoredUser() {
+﻿/**
+ * @returns {Object|null} The signed-in user from localStorage, or null.
+ */
+function getStoredUser() {
   return JSON.parse(localStorage.getItem('joinUser'));
 }
 
 
+/**
+ * Persists the signed-in user for other pages of the application.
+ *
+ * @param {Object} user - User object to store in localStorage.
+ */
 function saveStoredUser(user) {
   localStorage.setItem('joinUser', JSON.stringify(user));
 }
 
 
+/**
+ * Removes the stored user on logout.
+ */
 function clearStoredUser() {
   localStorage.removeItem('joinUser');
 }
 
 
+/**
+ * Escapes a value for safe interpolation into HTML templates.
+ *
+ * @param {*} value - Any value; it is converted to a string first.
+ * @returns {string} Text with HTML special characters encoded.
+ */
 function escapeHtmlText(value) {
   return String(value)
     .replaceAll("&", "&amp;")
