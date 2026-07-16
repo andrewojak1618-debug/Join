b4639d44ac08dcf029a0719ec067a8fcb993e472
ismael993-create <ismael.7@live.de>
2026-07-16 12:10:55 +0200

    merge konflikt

---
+++ components/js/firebase-config.js
@@ -0,0 +1,12 @@
+﻿window.joinFirebaseConfig = {
+  "projectId": "join-teamjob",
+  "appId": "1:740747769844:web:46d132f45d13061148aba1",
+  "databaseURL": "https://join-teamjob-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "join-teamjob.firebasestorage.app",
+  "apiKey": "AIzaSyCChX7sqa2jLBTqn0E-rvxzvaNYiq-dsF4",
+  "authDomain": "join-teamjob.firebaseapp.com",
+  "messagingSenderId": "740747769844",
+  "projectNumber": "740747769844",
+  "version": "2"
+};
+
