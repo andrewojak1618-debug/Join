c8fedf17fdd059f2a6a68aa3ee8455cd245c04bd
ismael993-create <ismael.7@live.de>
2026-07-19 18:28:25 +0200

    firebaseconfig example umbenannt

--- components/js/firebaseConfig.example.js
+++
@@ -1,8 +0,0 @@
-// Copy this file to firebaseConfig.js and replace the placeholder values.
-// Do not commit firebaseConfig.js with real project values.
-window.joinFirebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID",
-};
